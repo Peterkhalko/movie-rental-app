51fa1cdc4ab0bf1455dcdc276726224221828d04
Peterkhalko <mailmepeter2@gmail.com>
2022-03-17 16:12:21 +0530

    update routes

--- app.js
+++ app.js
@@ -1,84 +1,19 @@
 const express = require("express");
-const req = require("express/lib/request");
-const Joi = require("joi");
 const app = express();
+const mongoose = require("mongoose");
+const config = require("config");
 app.use(express.json());
-const port = 8000;
-let genres = [
-  { id: 1, name: "romance" },
-  {
-    id: 2,
-    name: "city light",
-  },
-];
-app.get("/api/genres", (req, res) => {
-  res.send(genres);
-});
-app.post("/api/genres", (req, res) => {
-  const genreExists = genres.find((e) => e.name === req.body.name);
-  if (genreExists) {
-    return res.status(404).send("Movie alredy exists");
-  }
-  const { error } = movieInputValidation(req.body);
-  if (error) {
-    console.log("error", error.details[0].message);
-    return res.status(400).send(error.details[0].message);
-  }
-  const newGenre = {
-    id: genres.length + 1,
-    name: req.body.name,
-  };
-  genres.push(newGenre);
-  res.send(newGenre);
-});
-
-app.put("/api/genres/:id", (req, res) => {
-  const id = req.params.id;
-  const genreExists = genres.find((e) => e.id === parseInt(id));
-  if (!genreExists) {
-    return res.status(404).send("movie not found to update");
-  }
-  const { error } = movieInputValidation(req.body);
-  if (error) {
-    console.log("error", error.details[0].message);
-    return res.status(400).send(error.details[0].message);
-  }
-  genreExists.name = req.body.name;
-  res.send(genreExists);
-});
-app.patch("/api/genres/:id", (req, res) => {
-  const id = req.params.id;
-  const genreExists = genres.find((e) => e.id === parseInt(id));
-  if (!genreExists) {
-    return res.status(404).send("movie not found to Patch");
-  }
-  const { error } = movieInputValidation(req.body);
-  if (error) {
-    console.log("error", error.details[0].message);
-    return res.status(400).send(error.details[0].message);
-  }
-  genreExists.name = req.body.name;
-  res.send(genreExists);
-});
-app.delete("/api/genres/:id", (req, res) => {
-  const id = req.params.id;
-  const genreExists = genres.find((e) => e.id === parseInt(id));
-  if (!genreExists) {
-    return res.status(404).send("movie not found to update");
-  }
-  let deletingIndex = genres.indexOf(genreExists);
-  genres.splice(deletingIndex, 1);
-  res.send(genreExists);
-  console.log("genres after deletion: ", genres);
-});
-
-function movieInputValidation(input) {
-  const schema = Joi.object({
-    name: Joi.string().min(3).max(20).required(),
+const port = process.env.PORT || 8000;
+const genresRouter = require("./routes/genresRoutes");
+app.use("/api/genres", genresRouter);
+mongoose
+  .connect(config.get("DB_CONNECTION_URL"))
+  .then((db) => {
+    console.log(`connected to ${config.get("DB_CONNECTION_URL")} db`);
+  })
+  .catch((error) => {
+    console.log(error);
   });
-  return schema.validate(input);
-}
-
 app.listen(port, () => {
   console.log(`Server is up and runnig! ${port}`);
 });
---
+++ routes/genresRoutes.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const Joi = require("joi");
+const router = express.Router();
+const mongoose = require("mongoose");
+const config = require("config");
+const { Genre, genreInputValidation } = require("../models/genreModel");
+
+//get API
+router.get("/", async (req, res) => {
+  try {
+    const genres = await Genre.find({});
+    if (!genres) {
+      return res.status(400).send("Data not found");
+    }
+    res.send(genres);
+  } catch (error) {
+    res.sendStatus(404).send(error);
+  }
+});
+//getById API
+router.get("/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const genre = await Genre.findById({ _id });
+    if (!genre) return res.status(404).send("Genre not found");
+    res.send(genre);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+//post API
+router.post("/", async (req, res) => {
+  try {
+    const { error } = genreInputValidation(req.body);
+    if (error) {
+      throw error.details[0].message;
+    }
+    const genre = new Genre({
+      name: req.body.name,
+    });
+    await genre.save();
+    res.send(genre);
+  } catch (error) {
+    res.status(409).send(error.message || error);
+  }
+});
+
+//Put API
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { error } = genreInputValidation(req.body);
+    if (error) {
+      throw error.details[0].message;
+    }
+    const genre = await Genre.findByIdAndUpdate(
+      id,
+      { $set: { name: req.body.name } },
+      { new: true }
+    );
+    if (!genre) {
+      throw "_id not found to update the data";
+    }
+    res.send(genre);
+  } catch (error) {
+    res.status(404).send(error.message || error);
+  }
+});
+// router.patch("/:id", (req, res) => {
+//   const id = req.params.id;
+//   const genreExists = genres.find((e) => e.id === parseInt(id));
+//   if (!genreExists) {
+//     return res.status(404).send("genre not found to Patch");
+//   }
+//   const { error } = genreInputValidation(req.body);
+//   if (error) {
+//     console.log("error", error.details[0].message);
+//     return res.status(400).send(error.details[0].message);
+//   }
+//   genreExists.name = req.body.name;
+//   res.send(genreExists);
+// });
+//Delete API
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const genre = await Genre.findByIdAndDelete(id);
+    if (!genre) {
+      throw "_id not found to delete";
+    }
+    res.send(genre);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+module.exports = router;
