c8cd2807e9e1f596d39162115b9f1ece8af82766
Peterkhalko <mailmepeter2@gmail.com>
2022-03-23 15:29:12 +0530

    worked on transactions on rental routes

--- routes/rentalRoutes.js
+++ routes/rentalRoutes.js
@@ -30,38 +30,68 @@ router.post("/", async (req, res) => {
     if (!movie || !customer) {
       return res.status(400).send("movie or customer not found");
     }
+    if (movie.numberInSock == 0)
+      return res.status(400).send("Movie out of stock");
+
     const rental = new Rentals({
       customer: {
         name: customer.name,
         phone: customer.phone,
+        _id: customer._id,
       },
       movie: {
         title: movie.title,
         dailyRentalRate: movie.dailyRentalRate,
+        _id: movie._id,
       },
       rentalFee: movie.dailyRentalRate * 10,
     });
-    await rental.save();
-    res.send(rental);
+    const session = await Rentals.startSession();
+    session.startTransaction();
+    try {
+      await rental.save();
+      await Movie.findByIdAndUpdate(movie._id, {
+        $inc: { numberInStocks: -1 },
+      });
+      session.commitTransaction();
+      session.endSession();
+      res.send(rental);
+    } catch (error) {
+      session.abortTransaction();
+      session.endSession();
+      return res.status(500).send("Something failed");
+    }
   } catch (error) {
     res.send(error);
   }
 });
 
 router.patch("/:id", async (req, res) => {
+  const _id = req.params.id;
+  const session = await Rentals.startSession();
+  session.startTransaction();
   try {
     const rental = await Rentals.findByIdAndUpdate(
       req.params.id,
       {
         $set: {
-          dateIn: Date.now(),
+          dateIn: req.body.currentDate,
         },
       },
       { new: true }
     );
+    const _id = rental.movie._id;
+    await Movie.findByIdAndUpdate(_id, {
+      $inc: { numberInStocks: 1 },
+    });
+
+    session.commitTransaction();
+    session.endSession();
     res.send(rental);
   } catch (error) {
-    res.send(error);
+    session.abortTransaction();
+    session.endSession();
+    return res.status(500).send(error);
   }
 });
 
