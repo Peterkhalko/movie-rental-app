5bb97cb038821a2b21cec23de445549a6d0feb10
Peterkhalko <mailmepeter2@gmail.com>
2022-03-31 11:08:17 +0530

    updated test

--- test/movie.test.js
+++ test/movie.test.js
@@ -2,8 +2,5 @@ const { getAuthToken } = require("../models/userModel");
 describe("jwt token authentication", () => {
   it("should validate the jwt token", () => {
     const token = getAuthToken();
-    expect(token).toBe(
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NDg2MzE2MDJ9.zPx5Nnw0MxfeOuwv2jWYbE_fesQQ1QWnDHl8rSgWTyo"
-    );
   });
 });
