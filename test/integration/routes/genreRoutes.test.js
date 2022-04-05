e4e88b9fad3ab2e7380cbf40910fc715bf8ee668
Peterkhalko <mailmepeter2@gmail.com>
2022-04-05 14:51:58 +0530

    update genre jest test scripts

--- test/integration/routes/genreRoutes.test.js
+++ test/integration/routes/genreRoutes.test.js
@@ -72,9 +72,7 @@ describe("/api/genres", () => {
     });
     it("should return the 400 if genre name character is more than 10 character", async () => {
       const user = new Users();
-      console.log(user);
       const token = user.getAuthToken();
-      console.log(token);
       const res = await req
         .post("/api/genres")
         .set("x-auth-token", token)
@@ -104,7 +102,159 @@ describe("/api/genres", () => {
       expect(res.body).toHaveProperty("name", "Drama");
     });
   });
-  describe("PUT /", () => {
-    it("should retune ", () => {});
+  describe("PUT /:id", () => {
+    it("should return 400 for invalid ObjectId type format ", async () => {
+      const id = new mongoose.Types.ObjectId();
+      const res = await req.put("/api/genres/", id);
+      expect(res.status).toBe(404);
+    });
+    it("should return 403 if token not found ", async () => {
+      const token = "A";
+      const res = await req
+        .put("/api/genres/624ad8d7be6fb5d80d6b9f7a")
+        .set("x-auth-toke", token);
+      expect(res.status).toBe(403);
+    });
+    it("should return 400 if put data is less than 3 character", async () => {
+      const user = new Users();
+      const token = user.getAuthToken();
+      const genre = new Genre({
+        name: "something",
+      });
+      await genre.save();
+      const res = await req
+        .put("/api/genres/" + genre._id)
+        .set("x-auth-token", token)
+        .send({ name: "pe" });
+      expect(res.status).toBe(400);
+    });
+    it("should return 400 if put data is more than 10 character", async () => {
+      const user = new Users();
+      const token = user.getAuthToken();
+      const genre = new Genre({
+        name: "something",
+      });
+      await genre.save();
+      const res = await req
+        .put("/api/genres/" + genre._id)
+        .set("x-auth-token", token)
+        .send({ name: "peter kahlko from nagpur" });
+      expect(res.status).toBe(400);
+    });
+    it("should return 400 if provided object id does not exists", async () => {
+      const user = new Users();
+      const token = user.getAuthToken();
+      const res = await req
+        .put("/api/genres/624ad8d7be6fb5d80d6b9f7a")
+        .set("x-auth-token", token)
+        .send({ name: "peter" });
+      expect(res.status).toBe(400);
+    });
+    it("should return 200 if put data successfull", async () => {
+      const user = new Users();
+      const token = user.getAuthToken();
+      const genre = new Genre({
+        name: "something",
+      });
+      await genre.save();
+      const res = await req
+        .put("/api/genres/" + genre._id)
+        .set("x-auth-token", token)
+        .send({ name: "peter" });
+      expect(res.status).toBe(200);
+    });
+    it("should return 200 if put data successfull", async () => {
+      const user = new Users();
+      const token = user.getAuthToken();
+      const genre = new Genre({
+        name: "something",
+      });
+      await genre.save();
+      const res = await req
+        .put("/api/genres/" + genre._id)
+        .set("x-auth-token", token)
+        .send({ name: "peter" });
+      expect(res.status).toBe(200);
+    });
+    it("should return put genre", async () => {
+      const user = new Users();
+      const token = user.getAuthToken();
+      const genre = new Genre({
+        name: "something",
+      });
+      await genre.save();
+      const res = await req
+        .put("/api/genres/" + genre._id)
+        .set("x-auth-token", token)
+        .send({ name: "peter" });
+      expect(res.body).toHaveProperty("name", "peter");
+    });
+  });
+  describe("DELETE /:id", () => {
+    it("should return 400 for invalid ObjectId", async () => {
+      const id = new mongoose.Types.ObjectId();
+      const res = await req.delete("/api/genres/", id);
+      expect(res.status).toBe(404);
+    });
+    it("should return 403 if token not found ", async () => {
+      const token = "A";
+      const res = await req
+        .delete("/api/genres/624ad8d7be6fb5d80d6b9f7a")
+        .set("x-auth-toke", token);
+      expect(res.status).toBe(403);
+    });
+    it("should return 403 if admin:true admin auth token not found ", async () => {
+      const user = new Users();
+      const token = user.getAuthToken();
+      const res = await req
+        .delete("/api/genres/624ad8d7be6fb5d80d6b9f7a")
+        .set("x-auth-token", token);
+      expect(res.status).toBe(403);
+    });
+    it("should return 400 for invalid ObjectId type format ", async () => {
+      const id = new mongoose.Types.ObjectId();
+      const res = await req.delete("/api/genres/", id);
+      expect(res.status).toBe(404);
+    });
+    it("should return 400 if provided object id does not exists", async () => {
+      const user = new Users({
+        isAdmin: true,
+      });
+      const token = user.getAuthToken();
+      const res = await req
+        .delete("/api/genres/624ad8d7be6fb5d80d6b9f7a")
+        .set("x-auth-token", token);
+      expect(res.status).toBe(400);
+    });
+    it("should return 200 if genre gets successfully deleted", async () => {
+      const user = new Users({
+        isAdmin: true,
+      });
+      const token = user.getAuthToken();
+      const genre = new Genre({
+        name: "Romantic",
+      });
+      await genre.save();
+      console.log(genre._id);
+      const res = await req
+        .delete("/api/genres/" + genre._id)
+        .set("x-auth-token", token);
+      expect(res.status).toBe(200);
+    });
+    it("should send genre back if genre gets successfully deleted", async () => {
+      const user = new Users({
+        isAdmin: true,
+      });
+      const token = user.getAuthToken();
+      const genre = new Genre({
+        name: "Romantic",
+      });
+      await genre.save();
+      console.log(genre._id);
+      const res = await req
+        .delete("/api/genres/" + genre._id)
+        .set("x-auth-token", token);
+      expect(res.body).toHaveProperty("name", "Romantic");
+    });
   });
 });
